2190880fff08d1718c2ef873ac7a1e39c81f086d
Tohirjon-Odilov <shahanshoh819@gmail.com>
2026-01-10 20:38:30 +0500

    add analytics configuration and integrate MathOperations component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MathOperations } from './math-operations/math-operations';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MathOperations],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
