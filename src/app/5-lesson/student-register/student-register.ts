8294ed4ddb4401d658517c72882d7d549df65499
Tohirjon-Odilov <shahanshoh819@gmail.com>
2026-01-31 22:03:57 +0500

    implement student registration form with validation; add password match validator and enhance styles

--- src/app/5-lesson/student-register/student-register.ts
+++ src/app/5-lesson/student-register/student-register.ts
@@ -1,10 +1,59 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { passwordMatchValidator } from '../custom-validator/password-match-validator';
+import { disabled } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-student-register',
-  imports: [],
-  template: `<p>student-register works!</p>`,
+  imports: [ReactiveFormsModule],
+  templateUrl: './student-register.html',
   styleUrl: './student-register.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StudentRegister { }
+export class StudentRegister implements OnInit {
+  private fb = inject(FormBuilder);
+  studentForm!: FormGroup;
+
+  ngOnInit() {
+    this.studentForm = this.fb.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        secondName: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required], { disabled: true }],
+      },
+      {
+        validators: passwordMatchValidator,
+      },
+    );
+  }
+
+  onSubmit() {
+    if (this.studentForm.valid) {
+      console.log(this.studentForm.value);
+      alert('Xabar yuborildi!');
+      this.studentForm.reset();
+    }
+  }
+
+  get name() {
+    return this.studentForm.get('name');
+  }
+
+  get secondName() {
+    return this.studentForm.get('secondName');
+  }
+
+  get email() {
+    return this.studentForm.get('email');
+  }
+
+  get password() {
+    return this.studentForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.studentForm.get('confirmPassword');
+  }
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { LoginForma } from "./5-lesson/other/login-forma/login-forma";
 import { RegisterForm } from "./5-lesson/other/register-form/register-form";
 import { ReactiveForma } from "./5-lesson/other/reactive-forma/reactive-forma";
+import { StudentRegister } from "./5-lesson/student-register/student-register";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginForma, RegisterForm, ReactiveForma],
+  imports: [RouterOutlet, LoginForma, RegisterForm, ReactiveForma, StudentRegister],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
