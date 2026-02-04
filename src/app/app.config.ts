728859aeb759b308a5a9f65ebc14bd9c726e8108
Tohirjon-Odilov <shahanshoh819@gmail.com>
2026-02-04 19:58:14 +0500

    add PrimeNG dependencies and configure animations; update student registration form styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      ripple: true
+    })
   ]
 };
